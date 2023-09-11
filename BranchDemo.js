b15b059cfc129e70c9f03274efa6e1cae4743b15
jztc21 <jztc21@gmail.com>
2023-09-11 12:22:40 -0400

    Create BranchDemo.js
    1. Added the initial source code for the branching demo

---
+++ BranchDemo.js
@@ -0,0 +1,11 @@
+// FileName: BranchDemo.js
+//Author: James Chen
+//Objective: Demonstrate how to use GitHub branches
+
+const months = ["January", "February", "March", "April", 
+"May", "June", "July", "August",
+const d = new Date()
+//getMonth() returns the month as an integer (0-11)
+let month = months[d.getMonth()]
+
+console.log("The month is "+month)
