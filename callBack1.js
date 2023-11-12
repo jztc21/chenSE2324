eef17915653d91ceadfb41393a344e58414e4def
jztc21 <jztc21@gmail.com>
2023-11-12 18:00:25 -0500

    deleted unecessary stuff

--- callBack1.js
+++
@@ -1,9 +0,0 @@
-//Define higher order function
-function getUserName(callback){
-    const userName=prompt("enter your name: ");
-        callback(userName);
-}
-function greeting(name){
-    document.getElementById('demo').innerHTML=`<h1>${name}</h1>`
-}
-
--- hello.js
+++
@@ -1,9 +0,0 @@
-function greeting() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log('Hello World');
-        resolve();
-      }, 2000);
-    });
-  }
-  greeting();
